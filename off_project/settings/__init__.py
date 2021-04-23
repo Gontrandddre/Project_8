24e75133104addcd938bba2bc034395327adab46
Gontrandddre <gontrandddre@gmail.com>
2021-04-23 18:11:10 +0200

    settings

--- off_project/settings/__init__.py
+++ off_project/settings/__init__.py
@@ -94,21 +94,23 @@ WSGI_APPLICATION = 'off_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'off',
+        'USER': 'stagiaire',
+        'HOST': '127.0.0.1',
+        'TEST': {
+            'NAME': 'test_off',
+        },
+    },
+}
+
 if os.environ.get('ENV') == 'PRODUCTION':
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'off',
-            'USER': 'stagiaire',
-            'HOST': '127.0.0.1',
-            'TEST': {
-                'NAME': 'test_off',
-            },
-        },
-    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
